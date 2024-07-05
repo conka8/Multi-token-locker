2379a76d988652e909b3dd0606eade4be5405340
conka8 <abdulrehman923712@gmail.com>
2024-07-05 17:46:01 +0300

    remove fking openzepelling depends

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,64 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./CustomOwnable.sol";
-
-contract TokenLocker is CustomOwnable {
-    using SafeERC20 for IERC20;
-
+contract TokenLocker {
+    address public owner;
+    
     struct Lock {
-        address recipient;
         uint256 unlockTimestamp; // seconds
         uint256 amount;
-        address token;
     }
 
     mapping(address => Lock[]) public locks;
 
-    constructor() Ownable(msg.sender) {}
+    event TokensLocked(address indexed recipient, uint256 unlockTimestamp, uint256 amount);
+    event TokensUnlocked(address indexed recipient, uint256 amount);
 
-    function lockTokens(address _recipient, uint256 _unlockTimestamp, uint256 _amount, address _token) external payable onlyOwner {
-        require(_recipient != address(0), "Invalid recipient address");
-        require(_amount > 0, "Amount must be greater than zero");
-
-        // Ensure the contract has enough tokens
-        IERC20 tokenContract = IERC20(_token);
-        require(tokenContract.balanceOf(address(this)) >= _amount, "Insufficient balance");
-
-        // Lock the tokens
-        locks[_recipient].push(Lock(_recipient, _unlockTimestamp, _amount, _token));
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
     }
 
-    function unlockTokens(address _recipient, address _token) external payable {
-        Lock[] storage userLocks = locks[_recipient];
-        uint256 totalUnlockedAmount;
-        uint256 length = userLocks.length;
-        uint256[] memory indicesToRemove = new uint256[](length);
-        uint256 removeCount = 0;
+    constructor() {
+        owner = msg.sender;
+    }
 
-        // get total amount
-        for (uint256 i = 0; i < length; i++) {
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        owner = newOwner;
+    }
+
+    function lockTokens(address _recipient, uint256 _unlockTimestamp) external payable onlyOwner {
+        require(_recipient != address(0), "Invalid recipient address");
+        require(msg.value > 0, "Amount must be greater than zero");
+
+        // Lock the tokens
+        locks[_recipient].push(Lock(_unlockTimestamp, msg.value));
+        emit TokensLocked(_recipient, _unlockTimestamp, msg.value);
+    }
+
+    function unlockTokens(address _recipient) external {
+        Lock[] storage userLocks = locks[_recipient];
+        uint256 totalUnlockedAmount = 0;
+        uint256 i = 0;
+
+        while (i < userLocks.length) {
             if (userLocks[i].unlockTimestamp <= block.timestamp) {
                 totalUnlockedAmount += userLocks[i].amount;
-                indicesToRemove[removeCount] = i;
-                removeCount++;
+                // Перемещаем последний элемент на место текущего
+                userLocks[i] = userLocks[userLocks.length - 1];
+                userLocks.pop();
+            } else {
+                i++;
             }
         }
 
         require(totalUnlockedAmount > 0, "No tokens to unlock");
 
-        // try transfer
-        IERC20(_token).safeTransfer(_recipient, totalUnlockedAmount);
+        // Transfer unlocked tokens to the recipient
+        payable(_recipient).transfer(totalUnlockedAmount);
+        emit TokensUnlocked(_recipient, totalUnlockedAmount);
+    }
 
-        for (uint256 i = 0; i < removeCount; i++) {
-            uint256 indexToRemove = indicesToRemove[i];
-            if (indexToRemove != userLocks.length - 1) {
-                userLocks[indexToRemove] = userLocks[userLocks.length - 1];
-            }
-            userLocks.pop();
-        }
+    function recoverTokens(uint256 _amount) external onlyOwner {
+        require(_amount <= address(this).balance, "Insufficient balance");
+        payable(owner).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
